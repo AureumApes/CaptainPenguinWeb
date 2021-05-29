7a0f77c502cb1ac0a2ae06b6811ae1643f36108e
AureumApes <janundartur@gmail.com>
2021-05-29 12:26:13 +0200

    Better Game Field

--- game/egg.js
+++ game/egg.js
@@ -37,9 +37,7 @@ export default class Egg {
     }
 
     rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
-        if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2){
-            return false;
-        }
-        return true;
+        return !(x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2);
+
     }
 }
